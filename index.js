deaa8faf1d311653b9cf840de48b9ce6bff59866
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-04-02 02:05:16 +0530

    idk smtg maybe

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 const port = 3000;
-app.get('/', (req,res)=>{
+app.get('/', (req,res)=>{ 
     res.render('home');
 })
-app.get('/FAQs', (req,res)=>{
+app.get('/FAQs', (req,res)=>{ 
     res.render('FAQs');
 })
 app.listen(port, ()=>{console.log(`listening on port : 3000`)})
--- main.py
+++
@@ -1,4 +0,0 @@
-import urllib, urllib.request
-url = 'http://export.arxiv.org/api/query?search_query=electron&start=0&max_results=20'
-data = urllib.request.urlopen(url)
-print(data.read().decode('utf-8'))
